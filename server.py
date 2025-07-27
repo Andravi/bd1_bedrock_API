2c3a95494f4311de6aaf5cf8ab2f8aad762c5878
Andravi <andravi75@gmail.com>
2025-07-27 17:07:56 -0300

    add documentation openApi

--- server.py
+++ server.py
@@ -1,6 +1,20 @@
 from app import create_app
+from flask_swagger_ui import get_swaggerui_blueprint
+
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/api_docs.yml'  # Coloque seu arquivo YAML na pasta static
 
 app = create_app()
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "API BedRock"
+    }
+)
+
+app.register_blueprint(swaggerui_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True)
